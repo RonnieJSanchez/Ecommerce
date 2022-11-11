431c706f047e47dd331516325fb93ccb36a6ba34
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-11-10 20:23:53 -0500

    connected to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from "express"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
+dotenv.config();
 
 const app = express();
+
+//db
+mongoose
+    .connect(process.env.MONGO_URI)
 
 app.get('/users', (req, res) => {
     res.json({
@@ -8,10 +17,10 @@ app.get('/users', (req, res) => {
     });
 });
 
-const port = process.eventNames.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(8000, () => {
-    console.log("Node server is running on port 8000");
+    console.log(`Node server is running on port 8000 ${port}`);
 });
 
 
