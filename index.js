93247c87c05a18d4bfba5e341037dee4e4c23ee3
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-11-11 14:37:46 -0500

    registered user validation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import morgan from "morgan";
 import authRoutes from "./routes/auth.js";
-import router from "./routes/auth.js";
+
 
 
 dotenv.config();
@@ -14,6 +15,9 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log("DB ERROR = ", err));
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json());
 
 // router middleware
 app.use('/api', authRoutes);
