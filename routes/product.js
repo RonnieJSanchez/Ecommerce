d09578c651f96488e25228401f299f9fda3e7c16
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-11-15 18:53:59 -0500

    Add and save new products

---
+++ routes/product.js
@@ -0,0 +1,13 @@
+import express from "express";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//middlewares
+import { requireSignin, isAdmin } from "../middlewares/auth.js";
+// controllers 
+import { create, list } from "../controllers/product.js";
+
+router.post("/product", requireSignin, isAdmin, formidable(), create);
+router.get("/products", list);
+export default router;
