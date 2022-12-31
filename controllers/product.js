d64d3347d9357b083d0b091083ed1b8bf00a6af1
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-12-31 12:27:07 -0500

    added routes for admin order management

--- controllers/product.js
+++ controllers/product.js
@@ -307,3 +307,14 @@ const decrementQuantity = async (cart) => {
     console.log(err);
   }
 };
+
+export const orderStatus = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const order = await Order.findByIdAndUpdate(orderId, { status });
+    res.json(order);
+  } catch (err) {
+    console.log(err);
+  }
+};
